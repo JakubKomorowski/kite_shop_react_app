1a73d548c9373265935e794f1be0d2ae8427680e
JakubKomorowski <jakub.komorowski01@gmail.com>
2020-11-09 19:36:20 +0100

    adding localData

---
+++ src/localData/index.js
@@ -0,0 +1,91 @@
+import boardImage1 from "../assets/images/board-1.jpg";
+import boardImage2 from "../assets/images/board-2.jpg";
+import kiteImage1 from "../assets/images/kite-1.jpg";
+import kiteImage2 from "../assets/images/kite-2.jpg";
+import wetsuitImage1 from "../assets/images/wetsuit-1.jpg";
+import wetsuitImage2 from "../assets/images/wetsuit-2.jpg";
+import harnessImage1 from "../assets/images/harness-1.jpg";
+import harnessImage2 from "../assets/images/harness-2.jpg";
+
+export const productsData = [
+  {
+    productName: "Crazyfly Raptor",
+    productPrice: 2299,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: boardImage1,
+    productCategory: "boards",
+    freeDelivery: true,
+  },
+  {
+    productName: "Nobile NHP 2020",
+    productPrice: 1899,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: boardImage2,
+    productCategory: "boards",
+    freeDelivery: false,
+  },
+  {
+    productName: "Cabrinha Switchblade 2020",
+    productPrice: 3399,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: kiteImage1,
+    productCategory: "kites",
+    freeDelivery: true,
+  },
+  {
+    productName: "Ozone Enduro V3",
+    productPrice: 4299,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: kiteImage2,
+    productCategory: "kites",
+    freeDelivery: true,
+  },
+  {
+    productName: "O'Neill Psycho Tech",
+    productPrice: 1499,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: wetsuitImage1,
+    productCategory: "wetsuits",
+    freeDelivery: false,
+  },
+  {
+    productName: "Manera Meteor X10D",
+    productPrice: 1599,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: wetsuitImage2,
+    productCategory: "wetsuits",
+    freeDelivery: false,
+  },
+  {
+    productName: "Mystic Majestic X",
+    productPrice: 2299,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: harnessImage1,
+    productCategory: "harnesses",
+    freeDelivery: true,
+  },
+  {
+    productName: "Ride Engine Elite Carbon",
+    productPrice: 2299,
+    productQuantity: 1,
+    productDescription:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sint consequuntur eius! Ea veniam porro obcaecati cumque sed repellat quaerat!",
+    productImage: harnessImage2,
+    productCategory: "harnesses",
+    freeDelivery: true,
+  },
+];
