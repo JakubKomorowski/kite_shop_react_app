85dee0deefb560b6699be03b2e6cbfd27973df72
JakubKomorowski <jakub.komorowski01@gmail.com>
2020-11-14 20:03:23 +0100

    adding payment alert

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Cart from "../components/Cart";
 import Navbar from "../components/navigations/Navbar";
 import GlobalStyle from "../theme/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { mainTheme } from "../theme/mainTheme";
+import PaymentAlert from "../components/PaymentAlert";
+import ShopContext from "../context";
 
 const MainTemplate = ({ children }) => {
+  const value = useContext(ShopContext);
+  const { isPaymentAlertOpen } = value;
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={mainTheme}>
         <Navbar />
+        {isPaymentAlertOpen ? <PaymentAlert /> : null}
+
         <Cart />
         {children}
       </ThemeProvider>
