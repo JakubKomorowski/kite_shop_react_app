59eeeee36fdf5a3be3355ccef8b7dfd59e4c78cb
JakubKomorowski <jakub.komorowski01@gmail.com>
2020-12-02 17:48:31 +0100

    changing images

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,16 +2,26 @@ import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/styles.css";
 import React from "react";
 import image1 from "../assets/images/harness-1.jpg";
+import kite_bg1 from "../assets/images/kite-bg1.jpg";
+import kite_bg2 from "../assets/images/kite-bg2.jpg";
+import kite_bg3 from "../assets/images/kite-bg3.jpg";
+import kite_bg4 from "../assets/images/kite-bg4.jpg";
 
 const Slider = () => {
   return (
     <AwesomeSlider>
       <div>
-        <img src={image1} alt="kite" />
+        <img src={kite_bg1} alt="kite" />
       </div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
+      <div>
+        <img src={kite_bg2} alt="kite" />
+      </div>
+      <div>
+        <img src={kite_bg3} alt="kite" />
+      </div>
+      <div>
+        <img src={kite_bg4} alt="kite" />
+      </div>
     </AwesomeSlider>
   );
 };
