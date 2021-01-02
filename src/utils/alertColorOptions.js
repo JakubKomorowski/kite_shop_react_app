75d600f33a53136e061e3ae664db402ce59552f6
JakubKomorowski <jakub.komorowski01@gmail.com>
2021-01-02 18:35:00 +0100

    adding utils

---
+++ src/utils/alertColorOptions.js
@@ -0,0 +1,7 @@
+export const alertColorOptions = {
+  default: "info",
+  error: "error",
+  messageSend: "info",
+  success: "success",
+  warning: "warning",
+};
---
+++ src/utils/localStorageGetters.js
@@ -0,0 +1,63 @@
+export const getCartFromLocalStorage = () => {
+  let localStorageCart;
+
+  if (localStorage.getItem("cart")) {
+    localStorageCart = JSON.parse(localStorage.getItem("cart"));
+  } else {
+    localStorageCart = [];
+  }
+
+  return localStorageCart;
+};
+
+export const getCartCounterFromLocalStorage = () => {
+  let localStorageCounter;
+
+  if (localStorage.getItem("cartCounter")) {
+    localStorageCounter = JSON.parse(localStorage.getItem("cartCounter"));
+  } else {
+    localStorageCounter = 0;
+  }
+  return localStorageCounter;
+};
+
+export const getSelectedProductFromLocalStorage = () => {
+  let selectedProductLocalStorage;
+
+  if (localStorage.getItem("selectedProduct")) {
+    selectedProductLocalStorage = JSON.parse(
+      localStorage.getItem("selectedProduct")
+    );
+  } else {
+    selectedProductLocalStorage = {};
+  }
+  return selectedProductLocalStorage;
+};
+
+export const getNotSelectedProductFromLocalStorage = () => {
+  let notSelectedProductLocalStorage;
+
+  if (localStorage.getItem("notSelectedProduct")) {
+    notSelectedProductLocalStorage = JSON.parse(
+      localStorage.getItem("notSelectedProduct")
+    );
+  } else {
+    notSelectedProductLocalStorage = [];
+  }
+
+  return notSelectedProductLocalStorage;
+};
+
+export const getSelectedCategoryFromLocalStorage = () => {
+  let selectedCategoryLocalStorage;
+
+  if (localStorage.getItem("selectedCategory")) {
+    selectedCategoryLocalStorage = JSON.parse(
+      localStorage.getItem("selectedCategory")
+    );
+  } else {
+    selectedCategoryLocalStorage = [];
+  }
+
+  return selectedCategoryLocalStorage;
+};
