7df697d60354117ce8d1ef3c3324b7926a0270f5
JakubKomorowski <jakub.komorowski01@gmail.com>
2021-01-02 18:27:50 +0100

    adding material-ui theme provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0088a9",
+    },
+    secondary: {
+      main: "#00e5ff",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <ThemeProvider theme={theme}>
+      <Root />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
