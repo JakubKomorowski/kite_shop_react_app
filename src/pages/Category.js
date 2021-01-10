9ea6c634e6653d427fbcc9a25c78ef590ae7c9e4
JakubKomorowski <jakub.komorowski01@gmail.com>
2021-01-10 01:44:44 +0100

    changed styling

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -2,22 +2,84 @@ import React, { useContext } from "react";
 import ShopContext from "../context";
 import Product from "../components/Product";
 import CartAlert from "../components/CartAlert";
-
 import {
   ProductsListUl,
   ProductsListWrapper,
 } from "../components/styledComponents/StyledProductsList";
-
 import FilterCategoryMenu from "../components/FilterCategoryMenu";
+import Sort from "../components/sort/Sort";
+import styled from "styled-components";
+import CartAlertWoSize from "../components/CartAlertWoSize";
+
+const FilterAndSortWrapper = styled.div`
+  display: flex;
+
+  @media (max-width: 760px) {
+    display: block;
+    margin: 0 auto;
+    width: 300px;
+  }
+`;
+
+const FilterDrawerWrapper = styled.div`
+  margin: 25px;
+  @media (max-width: 760px) {
+    margin: 0px;
+  }
+`;
+
+const SortWrapper = styled.div`
+  transition: all 0.4s ease-in-out;
+  transform: ${(props) => (props.open ? "translateX(210px)" : "0px")};
+`;
+
+const AllProductsTitle = styled.h1`
+  display: flex;
+  justify-content: center;
+  margin: 1rem;
+  text-transform: capitalize;
+`;
 
 const Category = () => {
   const value = useContext(ShopContext);
-  const { selectedCategory } = value;
+  const { selectedCategory, filterDrawerOpen, windowSize } = value;
 
   return (
     <>
-      <CartAlert />
-      <FilterCategoryMenu />
+      {windowSize ? (
+        <>
+          <AllProductsTitle>
+            {selectedCategory.map((product, i) => {
+              return <>{i === 0 ? product.productCategory : null}</>;
+            })}
+          </AllProductsTitle>
+          <FilterAndSortWrapper>
+            <FilterDrawerWrapper>
+              <FilterCategoryMenu />
+            </FilterDrawerWrapper>
+            <SortWrapper open={filterDrawerOpen}>
+              <Sort />
+            </SortWrapper>
+          </FilterAndSortWrapper>
+        </>
+      ) : (
+        <>
+          <FilterAndSortWrapper>
+            <FilterDrawerWrapper>
+              <FilterCategoryMenu />
+            </FilterDrawerWrapper>
+            <SortWrapper open={filterDrawerOpen}>
+              <Sort />
+            </SortWrapper>
+          </FilterAndSortWrapper>
+          <AllProductsTitle>
+            {selectedCategory.map((product, i) => {
+              return <>{i === 0 ? product.productCategory : null}</>;
+            })}
+          </AllProductsTitle>
+        </>
+      )}
+
       <ProductsListWrapper>
         <ProductsListUl>
           {selectedCategory.map((product) => {
@@ -29,6 +91,8 @@ const Category = () => {
           })}
         </ProductsListUl>
       </ProductsListWrapper>
+      <CartAlert />
+      <CartAlertWoSize />
     </>
   );
 };
