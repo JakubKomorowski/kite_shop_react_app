0ec356a06e34f0a24b9d5180620b2b6d6ae7f5bf
JakubKomorowski <jakub.komorowski01@gmail.com>
2021-01-10 01:44:10 +0100

    changed styling of the slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,12 +3,12 @@ import kite_bg1 from "../../assets/images/kite-bg1.jpg";
 import kite_bg2 from "../../assets/images/kite-bg2.jpg";
 import kite_bg3 from "../../assets/images/kite-bg3.jpg";
 import kite_bg4 from "../../assets/images/kite-bg4.jpg";
-import { StyledAwesomeSlider } from "../styledComponents/StyledSlider";
+import AwesomeSlider from "react-awesome-slider";
 import "./slider.scss";
 
 const Slider = () => {
   return (
-    <StyledAwesomeSlider>
+    <AwesomeSlider>
       <div>
         <img src={kite_bg1} alt="kite" />
       </div>
@@ -21,7 +21,7 @@ const Slider = () => {
       <div>
         <img src={kite_bg4} alt="kite" />
       </div>
-    </StyledAwesomeSlider>
+    </AwesomeSlider>
   );
 };
 
