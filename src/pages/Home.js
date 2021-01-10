0ef4dceb41cfe23acd7b5463bc93a607b2737431
JakubKomorowski <jakub.komorowski01@gmail.com>
2021-01-10 01:49:43 +0100

    added more components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
+import CartAlert from "../components/CartAlert";
+import HomePageCategoriesList from "../components/HomePageCategoriesList";
+import MostPopularProducts from "../components/MostPopularProducts";
 import Slider from "../components/Slider/Slider";
+import {
+  TitleContainer,
+  Wrapper,
+} from "../components/styledComponents/StyledHome";
+import { StyledCartAlertWoSize } from "../components/styledComponents/StyledSingleProduct";
 
 const Home = () => {
   return (
-    <div>
-      <Slider />
-    </div>
+    <>
+      <Wrapper>
+        <Slider />
+        <TitleContainer>
+          <h2>Most Popular Products</h2>
+        </TitleContainer>
+        <MostPopularProducts />
+        <TitleContainer>
+          <h2>Categories</h2>
+        </TitleContainer>
+        <HomePageCategoriesList />
+      </Wrapper>
+      <CartAlert />
+      <StyledCartAlertWoSize />
+    </>
   );
 };
 
